299838de14199f4be6a66a6d7c402a05a0e0bce8
mirvision <florangorr@gmail.com>
2024-07-14 18:57:27 +0500

    dataTypes

---
+++ dataTypes/data.py
@@ -0,0 +1,45 @@
+x = 5
+print(type(x))
+
+y = "Hello World!"
+print(type(y))
+
+a = 20.5
+print(type(a))
+
+w = 1j
+print(type(w))
+
+e = ["apple", "banana", "cherry"]	
+print(type(e))
+
+r = ("apple", "banana", "cherry")
+print(type(r))
+
+t = range(6)
+print(type(t))	
+
+u = {"name" : "John", "age" : 36}	
+print(type(u))
+
+i = {"apple", "banana", "cherry"}	
+print(type(i))
+
+o = frozenset({"apple", "banana", "cherry"})	
+print(type(o))	
+
+
+s = True	
+print(type(s))
+
+d = b"Hello"	
+print(type(s))
+
+f = bytearray(5)	
+print(type(f))
+
+g = memoryview(bytes(5))	
+print(type(g))
+		
+l = None	
+print(type(l))		
