018cd74e782d9a7e548ff30a62eaa18dfb71a2a6
mirvision <florangorr@gmail.com>
2024-07-27 21:30:54 +0500

    Set Python

--- Python Sets/set.py
+++ Python Sets/set.py
@@ -42,3 +42,39 @@ print(thisset)
 thisset = {"apple", "banana", "cherry", False, True, 0}
 
 print(thisset)
+
+
+#Get the Length of a Set
+#Чтобы определить, сколько элементов в наборе, используйте функцию len() .
+thisset = {"apple", "banana", "cherry"}
+
+print(len(thisset))
+
+
+#Set Items - Data Types
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+print('set1',set1)
+print('set2',set2)
+print('set3',set3)
+
+
+#Набор может содержать различные типы данных:
+
+set1 = {"abc", 34, True, 40, "male"}
+
+print(set1)
+
+#type()
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+#The set() Constructor
+
+#Для создания набора также можно использовать конструктор set() 
+
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
