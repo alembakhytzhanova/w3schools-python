496a41ff73396872a7f3b0d34e5d405e47ec01b7
mirvision <florangorr@gmail.com>
2024-07-27 17:55:37 +0500

    Join Tuple

---
+++ Python Tuples/joinTuples.py
@@ -0,0 +1,16 @@
+#Join Two Tuples
+
+#Для объединения двух или более кортежей можно использовать + оператор:
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+#Multiply Tuples
+
+#Если вы хотите умножить содержимое кортежа заданное количество раз, вы можете использовать оператор * :
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
